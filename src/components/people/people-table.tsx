2808b62f631bd157eb1cc6acfbf60b91741e31f1
Karim Shehadeh <kshehadeh@underarmour.com>
2025-10-06 06:09:31 -0400

    Fix build error

--- src/components/people/people-table.tsx
+++ src/components/people/people-table.tsx
@@ -618,7 +618,6 @@ export function PeopleTable({ people, filteredPeople }: PeopleTableProps) {
                 managerId: null,
                 jobRoleId: null,
                 startedAt: null,
-                employeeType: null,
               }}
               onSuccess={handleFeedbackSuccess}
               onCancel={handleFeedbackCancel}
