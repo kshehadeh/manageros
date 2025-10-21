2dea9bb23200283e76f7a120d9ae3b47eee40a66
Karim Shehadeh <kshehadeh@underarmour.com>
2025-10-21 06:11:44 -0400

    Allow manifest.json to be served without auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -64,6 +64,6 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    '/((?!api/auth|_next/static|_next/image|favicon.ico|images/|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.svg|.*\\.ico).*)',
+    '/((?!api/auth|_next/static|_next/image|favicon.ico|manifest.json|images/|.*\\.png|.*\\.jpg|.*\\.jpeg|.*\\.gif|.*\\.svg|.*\\.ico).*)',
   ],
 }
